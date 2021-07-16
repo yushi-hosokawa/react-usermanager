7e9a7c302aad5f9ffe21a6e34deb197ca0eadc43
yushi hosokawa <49804320+yushi-hosokawa@users.noreply.github.com>
2021-07-16 16:57:30 +0900

    メッセージ機能の追加

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
-
+  const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
 
   const login = useCallback(
@@ -17,15 +19,18 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました．", status: "success" });
             history.push("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
-        .catch(() => alert("ログインできません"))
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
   return { login, loading };
 };
